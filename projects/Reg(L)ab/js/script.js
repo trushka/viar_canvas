aaa5946f88f842dfb83e94ed70476a351526d761
aBabken <ababken10@gmail.com>
2021-09-20 23:35:23 +0400

    ertgergf

--- projects/Reg(L)ab/js/script.js
+++ projects/Reg(L)ab/js/script.js
@@ -24,27 +24,97 @@ $(document).ready(function () {
     mediaActions();
   };
 
-
-
-  var header = document.querySelector(".header-main");    
+  var header = document.querySelector(".header-main");
   var sticky = header.offsetTop;
-  
-  window.onscroll = function() {
+
+  window.onscroll = function () {
     if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
-  }
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  };
   function mediaActions() {
     if (mediaChecker("min", 526)) {
       $(".collapse-block").slideUp();
     }
 
-
-
-
+    // if (mediaChecker("max", 600)) {
+    //   if (!($(".activity-item").parent().is(".activity--slider"))) {
+    //     $(".activity-item").wrapAll("<div class='activity--slider'></div>");
+    //   }
+    // } else {
+    //   if ($(".activity-item").parent().is(".activity--slider")) {
+    //     $(".activity-item").unwrap();
+    //   }
+    // }
   }
+
+  let machineSlider = $(".machine__items");
+  let initialCount = machineSlider.find(".machine__item").length;
+  let progressBarLabel = $(".machine-progress .progress-item");
+  let status = $(".machine-progress .progress-count");
+  progressBarLabel.css("width", (1 / initialCount) * 100 + "%");
+  machineSlider.on(
+    "beforeChange init reInit afterChange",
+    function (event, slick, currentSlide, nextSlide) {
+      var calc = ((nextSlide + 1) / slick.slideCount) * 100;
+
+      progressBarLabel.css("width", calc + "%").attr("aria-valuenow", calc);
+
+      var i = (currentSlide ? currentSlide : 0) + 1;
+
+      status.text(i + "/" + initialCount);
+    }
+  );
+
+  machineSlider.slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 5000,
+        settings: "unslick",
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: true,
+        },
+      },
+    ],
+  });
+
+  let activitySlider = $(".activity--slider");
+  let initialCount2 = activitySlider.find(".activity-item").length;
+  let progressBarLabel2 = $(".activity-progress .progress-item");
+  let status2 = $(".activity-progress .progress-count");
+  progressBarLabel2.css("width", (1 / initialCount2) * 100 + "%");
+  activitySlider.on(
+    "beforeChange init reInit afterChange",
+    function (event, slick, currentSlide, nextSlide) {
+      var calc = ((nextSlide + 1) / slick.slideCount) * 100;
+
+      progressBarLabel2.css("width", calc + "%").attr("aria-valuenow", calc);
+
+      var i = (currentSlide ? currentSlide : 0) + 1;
+
+      status2.text(i + "/" + initialCount2);
+    }
+  );
+
+  activitySlider.slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: false,
+    infinite: false,
+    variableWidth: true,
+    touchThreshold: 200
+
+  });
 
   function mediaChecker(max_min, resolution, width = "width") {
     return window.matchMedia(`(${max_min}-${width}: ${resolution}px)`).matches;
